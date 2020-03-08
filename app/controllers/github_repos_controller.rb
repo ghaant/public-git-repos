f6b18aaef54617b3db2b885fdf257fd0925d6e11
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2020-03-08 12:10:20 +0100

    Raise 406 error in case of a wrong parameter.

--- app/controllers/github_repos_controller.rb
+++ app/controllers/github_repos_controller.rb
@@ -1,8 +1,18 @@
+# frozen_string_literal: true
+
 class GithubReposController < ApplicationController
   def index
     client = Github::Client.new
 
-    response = client.user_public_repos(params[:owner])
+    if params[:filters]&.key?(:owner)
+      search_value = params[:filters][:owner]
+    elsif params[:filters].nil?
+      search_value = ''
+    else
+      render json: { message: "A wrong parameter name", status: 406 } and return
+    end
+
+    response = client.user_public_repos(search_value)
 
     render json: response.body
   end
